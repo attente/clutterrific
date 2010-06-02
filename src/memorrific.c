32a3a13ba495566e1b0f74a79cd526ae68679b2b
William Hua <huaw@mcmaster.ca>
2010-06-02 11:17:38 -0400

    some framework for different styles

--- src/memorrific.c
+++ src/memorrific.c
@@ -18,15 +18,31 @@
  * 02110-1301, USA.
  */
 
+/*
+ * To do:
+ *
+ * - number of colour schemes should be SCHEMES
+ */
 
 
-#define SCHEMES            9
 
-#define FADE_TIME       5000
+#define SCHEMES                  9
 
-#define MIN_THEME_TIME 15000
+#define FADE_TIME             5000
 
-#define MAX_THEME_TIME 25000
+#define MIN_THEME_TIME       15000
+
+#define MAX_THEME_TIME       25000
+
+#define STYLES                   1
+
+#define SWIPE_STYLE_MIN_TIME 20000
+
+#define SWIPE_STYLE_MAX_TIME 30000
+
+#define SWIPE_STYLE_MIN_LIFE  3000
+
+#define SWIPE_STYLE_MAX_LIFE  7000
 
 
 
@@ -67,6 +83,58 @@ typedef FineColour (*ColourScheme) (const FineColour *base,
 
 
 
+/*
+ * Each style is responsible for restarting the timeline on completion.
+ */
+
+typedef struct
+{
+  GCallback start;
+
+  GCallback update;
+
+  GCallback finish;
+
+  gfloat    min_time;
+
+  gfloat    max_time;
+}
+Style;
+
+
+
+typedef struct
+{
+  gfloat x[2];
+
+  gfloat y[2];
+}
+Rectangle;
+
+
+
+typedef struct
+{
+  Rectangle r[4];
+
+  gfloat    z[2];
+
+  gfloat    t[2];
+}
+Frame;
+
+
+
+typedef struct
+{
+  Frame         frame;
+
+  ClutterActor *actor;
+}
+ColourSwatch;
+
+
+
 static gfloat       width;
 
 static gfloat       height;
@@ -82,6 +150,8 @@ static ColourScheme scheme;
 static FineColour   scheme_colour;
 
 static gfloat       scheme_parameter;
+
+static Style        STYLE[STYLES];
 
 
 
@@ -206,11 +276,71 @@ get_colour (const FineColour *data)
 
 
 
+static Rectangle
+interpolate (const Rectangle *r0,
+             const Rectangle *r1,
+             gfloat           t)
+{
+  Rectangle r;
+
+  r.x[0] = r0->x[0] + t * (r1->x[0] - r0->x[0]);
+  r.y[0] = r0->y[0] + t * (r1->y[0] - r0->y[0]);
+  r.x[1] = r0->x[1] + t * (r1->x[1] - r0->x[1]);
+  r.y[1] = r0->y[1] + t * (r1->y[1] - r0->y[1]);
+
+  return r;
+}
+
+
+
+static Rectangle
+get_frame (const Frame *f,
+           gfloat       t)
+{
+  const Rectangle *r0;
+  const Rectangle *r1;
+
+  if (t < f->t[0])
+  {
+    r0 = f->r + 0;
+    r1 = f->r + 1;
+
+    t = (t - 0) / (f->t[0] - 0);
+  }
+  else if (t < f->t[1])
+  {
+    r0 = f->r + 1;
+    r1 = f->r + 2;
+
+    t = (t - f->t[0]) / (f->t[1] - f->t[0]);
+  }
+  else
+  {
+    r0 = f->r + 2;
+    r1 = f->r + 3;
+
+    t = (t - f->t[1]) / (1 - f->t[1]);
+  }
+
+  return interpolate (r0, r1, t);
+}
+
+
+
+static gfloat
+get_depth (const Frame *f,
+           gfloat       t)
+{
+  return f->z[0] + t * (f->z[1] - f->z[0]);
+}
+
+
+
 static void
 select_theme (ClutterTimeline *timeline,
               gpointer         data)
 {
-  scheme           = SCHEME[g_random_int_range (0, 1/*SCHEMES*/)];
+  scheme           = SCHEME[g_random_int_range (0, 1 /* SCHEMES */)];
   scheme_parameter = g_random_double ();
   stage_colour     = target_colour;
   scheme_colour    = scheme (NULL, NULL, scheme_parameter);
@@ -255,18 +385,57 @@ update_theme (ClutterTimeline *timeline,
 
 
 static void
-repeat_theme (ClutterTimeline *timeline,
-              gpointer         data)
+start_theme (ClutterTimeline *timeline,
+             gpointer         data)
 {
-  g_object_unref (timeline);
+  if (timeline != NULL)
+    g_object_unref (timeline);
 
   timeline = clutter_timeline_new (g_random_int_range (MIN_THEME_TIME, MAX_THEME_TIME + 1));
 
-  g_signal_connect (timeline, "started",   G_CALLBACK (select_theme), NULL);
-  g_signal_connect (timeline, "new-frame", G_CALLBACK (update_theme), NULL);
-  g_signal_connect (timeline, "completed", G_CALLBACK (repeat_theme), NULL);
+  g_signal_connect       (timeline, "started",   G_CALLBACK (select_theme), NULL);
+  g_signal_connect       (timeline, "new-frame", G_CALLBACK (update_theme), NULL);
+  g_signal_connect_after (timeline, "completed", G_CALLBACK (start_theme),  NULL);
 
   clutter_timeline_start (timeline);
+}
+
+
+
+static void
+start_style (ClutterTimeline *timeline,
+             const Style     *style)
+{
+  if (timeline != NULL)
+    g_object_unref (timeline);
+
+  timeline = clutter_timeline_new (g_random_int_range (style->min_time, style->max_time + 1));
+
+  if (style->start != NULL)
+    g_signal_connect       (timeline, "started",   style->start,  NULL);
+  if (style->update != NULL)
+    g_signal_connect       (timeline, "new-frame", style->update, NULL);
+  if (style->finish != NULL)
+    g_signal_connect_after (timeline, "completed", style->finish, NULL);
+
+  clutter_timeline_start (timeline);
+}
+
+
+
+static void
+swipe_style_start (ClutterTimeline *timeline,
+                   gpointer         data)
+{
+}
+
+
+
+static void
+swipe_style_finish (ClutterTimeline *timeline,
+                    gpointer         data)
+{
+  start_style (timeline, STYLE + g_random_int_range (0, STYLES));
 }
 
 
@@ -280,6 +449,8 @@ main (int   argc,
   clutter_init      (&argc, &argv);
   clutterrific_init (&argc, &argv);
 
+  cogl_set_depth_test_enabled (TRUE);
+
   SCHEME[0] = get_random_achromatic_colour;
   SCHEME[1] = get_random_monochromatic_colour;
   SCHEME[2] = get_random_analogous_colour;
@@ -289,6 +460,12 @@ main (int   argc,
   SCHEME[6] = get_random_neutral_colour;
   SCHEME[7] = get_random_warm_colour;
   SCHEME[8] = get_random_cool_colour;
+
+  STYLE[0].start    = G_CALLBACK (swipe_style_start);
+  STYLE[0].update   = NULL;
+  STYLE[0].finish   = G_CALLBACK (swipe_style_finish);
+  STYLE[0].min_time = SWIPE_STYLE_MIN_TIME;
+  STYLE[0].max_time = SWIPE_STYLE_MAX_TIME;
 
   stage_colour.h = 0;
   stage_colour.s = 0;
@@ -305,15 +482,8 @@ main (int   argc,
     clutter_actor_get_size  (stage, &width, &height);
   }
 
-  {
-    ClutterTimeline *timeline = clutter_timeline_new (g_random_int_range (MIN_THEME_TIME, MAX_THEME_TIME + 1));
-
-    g_signal_connect (timeline, "started",   G_CALLBACK (select_theme), NULL);
-    g_signal_connect (timeline, "new-frame", G_CALLBACK (update_theme), NULL);
-    g_signal_connect (timeline, "completed", G_CALLBACK (repeat_theme), NULL);
-
-    clutter_timeline_start (timeline);
-  }
+  start_theme (NULL, NULL);
+  start_style (NULL, STYLE);
 
   clutter_main ();
 
