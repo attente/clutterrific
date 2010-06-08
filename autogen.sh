a8571969f8e8c3ccb636d4390910089ffde30760
William Hua <huaw@mcmaster.ca>
2010-06-08 10:35:46 -0400

    rewrite autogen.sh based on dconf

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
-autoreconf
-./configure
+CRAP="aclocal.m4
+      autom4te.cache
+      config.log
+      config.status
+      configure
+      depcomp
+      INSTALL
+      install-sh
+      Makefile
+      Makefile.in
+      missing
+      src/config.h
+      src/config.h.in
+      src/Makefile
+      src/Makefile.in
+      src/stamp-h1"
+
+if [ "$1" = "clean" ]; then
+  rm -rf ${CRAP}
+  exit
+fi
+
+aclocal
+autoconf
+autoheader
+automake --add-missing
+
+./configure "$@"
